cb7217a7e1fe878deaf146492d38fc373a217ddf
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-11-10 13:32:27 -0600

    Added toolbox code

---
+++ toolbox/states.py
@@ -0,0 +1,150 @@
+import signal
+from multiprocessing import current_process
+
+import torch
+
+
+class State:
+    def __init__(self, attrib, dump_fn=lambda x: x, load_fn=lambda x: x):
+        """
+        Wrap this around the attribute you want to track in __init__ of a
+        Trackable subclass
+        Args:
+            attrib: The attribute you want to track
+            dump_fn: The function for converting the attribute to a serializable
+                     form
+            load_fn: Convert the serializable form back to the original value
+        """
+        self._attrib = attrib
+        self._dump_fn = dump_fn
+        self._load_fn = load_fn
+
+    @property
+    def attrib(self):
+        return self._attrib
+
+    def dump(self, curr_val):
+        return self._dump_fn(curr_val)
+
+    def load(self, dumped_val):
+        return self._load_fn(dumped_val)
+
+
+class TorchState(State):
+    """
+    A more specialized state wrapper for PyTorch objects who have state_dict()
+    function.
+    """
+
+    def __init__(self, attrib):
+        def load_fn(state_dict):
+            attrib.load_state_dict(state_dict)
+            return attrib
+
+        super().__init__(attrib, dump_fn=lambda x: x.state_dict(),
+                         load_fn=load_fn)
+
+
+class TrackableMeta(type):
+    """
+    Metaclass for registering objects.
+    """
+
+    def __new__(cls, cls_name, bases, attr):
+        state_dict = {}
+
+        def set_attr(self, attr_name, value):
+            if issubclass(value.__class__, State):
+                state_dict[attr_name] = value
+                value = value.attrib
+            object.__setattr__(self, attr_name, value)
+
+        def save_state(self, save_path=None):
+            attrs = self.__dict__
+            out_dict = {}
+            for attr_name, state_obj in state_dict.items():
+                value = state_obj.dump(attrs[attr_name])
+                out_dict[attr_name] = value
+            if save_path is not None:
+                torch.save(out_dict, save_path)
+            return out_dict
+
+        def load_state(self, in_dict, ignore_missing=False):
+            for attr_name, value in in_dict.items():
+                if not (attr_name in state_dict):
+                    print('attribute %s is not part of the object' % attr_name)
+                    if ignore_missing:
+                        print('abort loading')
+                        return
+                value = state_dict[attr_name].load(value)
+                self.__dict__[attr_name] = value
+
+        attr['__setattr__'] = set_attr
+        attr['save_state'] = save_state
+        attr['load_state'] = load_state
+        return super().__new__(cls, cls_name, bases, attr)
+
+
+class Trackable(State, metaclass=TrackableMeta):
+    """
+    The Trackable base class. If you want to automatically save the state of the
+    attributes, you should subclass this class. For each tracked state, wrap it
+    with a State object when initialized in __init__.
+    """
+
+    @property
+    def attrib(self):
+        return self
+
+    def dump(self, curr_val):
+        return curr_val.save_state()
+
+    def load(self, dumped_val):
+        self.load_state(dumped_val)
+        return self
+
+
+def save_on_interrupt(save_path=None, exception_handling=None):
+    """
+    The save on interrupt decorator. For any (member) function decorated, it
+    will capture SIG_INT and save the state.
+    Args:
+        save_path: The path for saving the state
+        exception_handling: A function with only parameter self. Called when
+        signal is received.
+
+    Returns: The decorated function contains the original function.
+    """
+
+    def decorator(func):
+        current_process_name = current_process().name
+
+        def wrapper(self, *args, **kwargs):
+            process_name = current_process().name
+
+            def handler(sig, frame):
+                if current_process().name == process_name:
+                    if exception_handling is not None:
+                        exception_handling(self)
+                    self.save_state(save_path)
+                    raise KeyboardInterrupt()
+
+            top_level = 'nested_tracked_state' not in globals()
+            if top_level:
+                global nested_tracked_state
+                nested_tracked_state = True
+                old_handler = signal.signal(signal.SIGINT, handler)
+
+            out = None
+            try:
+                out = func(self, *args, **kwargs)
+            except KeyboardInterrupt:
+                pass
+            if top_level:
+                signal.signal(signal.SIGINT, old_handler)
+                del nested_tracked_state
+            return out
+
+        return wrapper
+
+    return decorator
---
+++ toolbox/train.py
@@ -0,0 +1,108 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+from toolbox.states import Trackable, TorchState, State, save_on_interrupt
+from util.helpers import save_model
+from .progress_bar import ProgressBar
+
+
+class TrackedTraining(Trackable):
+    def __init__(self, model: nn.Module, train_dataset, valid_dataset,
+                 optimizer_cls, model_save_path_prefix,
+                 state_save_path_prefix, optimizer_config: dict,
+                 train_loader_config: dict, inference_loader_config: dict,
+                 epochs=1, gpu=True, progress_bar_size=20):
+        self.model = TorchState(model)
+        self.optimizer_cls = optimizer_cls
+        self.optimizer = TorchState(
+            optimizer_cls(filter(lambda p: p.requires_grad, model.parameters()),
+                          **optimizer_config))
+        self.train_dataset = train_dataset
+        self.valid_dataset = valid_dataset
+
+        # Avoid saving sampler twice
+        self.train_sampler = train_loader_config.pop('sampler', None)
+
+        self.model_save_path = model_save_path_prefix
+        self.state_save_path = state_save_path_prefix
+        self.epochs = epochs
+        self.curr_epochs = State(0)
+        self.curr_steps = State(0)
+        self.train_loader_config = State(train_loader_config)
+        self.inference_loader_config = State(inference_loader_config)
+        self.gpu = gpu
+        self.progress_bar_size = State(progress_bar_size)
+
+    def parse_batch(self, batch):
+        return torch.Tensor(0.0), torch.Tensor(0.0)
+
+    def loss_fn(self, output, target):
+        return torch.Tensor(0.0)
+
+    def validate(self, data_loader):
+        total_data = 0
+        total_loss = 0
+        self.model.eval()
+        with torch.no_grad():
+            for i, batch in enumerate(data_loader):
+                ipt, target = self.parse_batch(batch)
+                output = self.model(ipt)
+                batch_size = output.shape[0]
+                loss = self.loss_fn(output, target)
+                total_loss += loss * batch_size
+                total_data += batch_size
+        return total_loss / total_data
+
+    def train(self):
+        @save_on_interrupt(self.state_save_path + '_interrupt.state')
+        def _train(self):
+            if torch.cuda.is_available() and self.gpu:
+                self.model.cuda()
+
+                # Manually moving optimizer state to GPU
+                # https://github.com/pytorch/pytorch/issues/2830#issuecomment-336194949
+                for state in self.optimizer.state.values():
+                    for k, v in state.items():
+                        if isinstance(v, torch.Tensor):
+                            state[k] = v.cuda()
+                torch.cuda.empty_cache()
+
+            train_loader = DataLoader(self.train_dataset,
+                                      sampler=self.train_sampler,
+                                      **self.train_loader_config)
+            valid_loader = DataLoader(self.valid_dataset,
+                                      **self.inference_loader_config)
+
+            while self.curr_epochs < self.epochs:
+                self.model.train()
+                total_steps = self.curr_steps + len(train_loader)
+                progress_bar = ProgressBar(self.progress_bar_size,
+                                           ' loss: %.03f, batch: %d')
+                for _, batch in enumerate(train_loader):
+                    ipt, target = self.parse_batch(batch)
+                    output = self.model(ipt)
+                    loss = self.loss_fn(output, target)
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+                    progress_bar.progress(
+                        self.curr_steps / total_steps * 100,
+                        loss, self.curr_steps)
+                    self.curr_steps += 1
+                self.curr_epochs += 1
+                self.curr_steps = 0
+
+                save_model(self.model,
+                           '%s_%d.model' % (
+                               self.model_save_path, self.curr_epochs),
+                           self.curr_epochs, 0)
+                self.save_state(
+                    save_path='%s_%d.state' % (
+                        self.state_save_path, self.curr_epochs))
+                validate_loss = self.validate(valid_loader)
+                print('\nValidation loss: %f' % validate_loss)
+
+            return self.model
+
+        return _train(self)
