78bea84edf22a851ad5ad896335ac4bded8933c6
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-11-12 23:35:52 -0600

    Removed Alpha

--- test_inference.py
+++ test_inference.py
@@ -74,8 +74,7 @@ def test(model: nn.Module, data_loader: DataLoader, save_path: str):
                 out_img[:, :, 1] = output[j, 0]
                 out_img[:, :, 2] = output[j, 0]
                 r, g, b = cv2.split(out_img)
-                alpha = np.ones((128, 128))
-                out_img = cv2.merge((r, g, b, alpha))
+                out_img = cv2.merge((r, g, b))
                 cv2.imwrite(os.path.join(save_path, 'test_%05d.png' % idx),
                             out_img)
             progress_bar.progress(i / total_steps * 100, i)
