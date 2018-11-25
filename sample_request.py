2e9d8683b4499318544facc36a53c608b7fd6d94
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-11-25 13:34:00 -0600

    Changed sample_request to save result each time

--- sample_request.py
+++ sample_request.py
@@ -1,24 +1,39 @@
 import argparse
+import datetime
 import os
+
 import requests
 
 """
-Sends all images in a folder to the autograder server.
+Sends all images in a folder to the auto grader server.
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--netid", required=True, help="student's NetID")
-parser.add_argument("--token", required=True, help="student's token")
-parser.add_argument("--image-dir", required=True, help="submission directory of 128x128 images")
-parser.add_argument("--server", required=True, help="IP address of grading server")
+parser.add_argument('--netid', required=True, help='student\'s NetID')
+parser.add_argument('--token', required=True, help='student\'s token')
+parser.add_argument('--image-dir', required=True,
+                    help='submission directory of 128x128 images')
+parser.add_argument('--server', required=True,
+                    help='IP address of grading server')
+parser.add_argument('--history-csv', required=True,
+                    help='the place to save the history')
+parser.add_argument('--comment', required=True,
+                    help='comment about this submission')
 args = parser.parse_args()
 
 images = [x for x in os.listdir(args.image_dir)]
 files = {}
 for image in images:
-    with open(os.path.join(args.image_dir, image), "rb") as bin_data:
+    with open(os.path.join(args.image_dir, image), 'rb') as bin_data:
         files[image] = bin_data.read()
 
 payload = {"netid": args.netid, "token": args.token}
 res = requests.post(args.server, files=files, data=payload)
+with open(args.history_csv, 'r') as h:
+    history_lines = h.readlines()
+    if len(history_lines) == 0:
+        h.write('Time, Result, Comment\n')
+    now = datetime.datetime.now()
+    h.write(
+        '%s, %s, %s' % (now.strftime("%Y-%m-%d %H:%M"), res.text, args.comment))
 print(res.text)
