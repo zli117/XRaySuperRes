0d5fbb97f63251382c7498bd466c1595e8d35ec3
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-11-27 00:27:02 -0600

    Changed save prefix

--- experiment7.py
+++ experiment7.py
@@ -97,9 +97,8 @@ with torch.cuda.device_ctx_manager(args.device):
     optimizer_config = {'lr': 5e-6}
     dncnn = DnCNN(1, tanh_out=True, built_in_residual=True)
     discriminator = Discriminator()
-    save_pfx = args.save_dir + 'dncnn'
     train = TrainDenoise(discriminator, dncnn, train_dataset, valid_dataset,
-                         Adam, save_pfx, optimizer_config,
+                         Adam, args.save_dir, optimizer_config,
                          train_loader_config, inference_loader_config,
                          epochs=args.epochs, save_optimizer=False)
     if args.denoise_state_path is not None:
