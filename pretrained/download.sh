6a5c78096f372ffaf26813492ffc4f540dfc21a8
Zonglin Li <development.my6565@gmail.com>
2018-11-29 16:58:44 -0600

    Updated download script for pretrained model

--- pretrained/download.sh
+++ pretrained/download.sh
@@ -1,1 +1,6 @@
-wget https://download.pytorch.org/models/vgg16-397923af.pth
+if [ ! -f vgg16-397923af.pth ]; then
+  wget https://download.pytorch.org/models/vgg16-397923af.pth
+fi
+if [ ! -f vgg11-bbd30ac9.pth ]; then
+  wget https://download.pytorch.org/models/vgg11-bbd30ac9.pth
+fi
