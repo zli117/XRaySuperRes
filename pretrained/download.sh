afe76dab9fd00e8dbec159f8e8d21059289e2d0c
Zonglin Li <development.my6565@gmail.com>
2018-12-12 16:53:26 -0600

    Updated download sh

--- pretrained/download.sh
+++ pretrained/download.sh
@@ -4,3 +4,6 @@ fi
 if [ ! -f vgg11-bbd30ac9.pth ]; then
   wget https://download.pytorch.org/models/vgg11-bbd30ac9.pth
 fi
+if [ ! -f vgg19-dcbb9e9d.pth ]; then
+  wget https://download.pytorch.org/models/vgg19-dcbb9e9d.pth
+fi
