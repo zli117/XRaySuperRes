3e4b4842fc5399dcfc5e500e98163db15bad08ff
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-12-19 17:58:11 -0600

    Removed unused srresnet implementation

--- model/srgan.py
+++ model/srgan.py
@@ -7,10 +7,10 @@ from torchvision.models import vgg19
 
 
 def weights_init_normal(m):
-    classname = m.__class__.__name__
-    if classname.find('Conv') != -1:
+    class_name = m.__class__.__name__
+    if class_name.find('Conv') != -1:
         torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
-    elif classname.find('BatchNorm') != -1:
+    elif class_name.find('BatchNorm') != -1:
         torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
         torch.nn.init.constant_(m.bias.data, 0.0)
 
